b68e884f0c3645498bd2751b0c58e99605a6c1b9
arthurogg <negoumarthuro@gmail.com>
2026-03-29 19:50:27 +0200

    initial commit

---
+++ spy-game/src/test/java/com/projet/spy_game/SpyGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.spy_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpyGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
